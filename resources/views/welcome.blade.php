10032a1fde9466558f6f08029e0390235fdd9cad
nachool <nachma.olivia1394@gmail.com>
2026-02-21 10:28:06 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pertemuan 1</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-[#0f0f0f] min-h-screen flex items-center justify-center p-6">
+
+    <div class="w-full max-w-4xl border border-neutral-800 bg-[#171717] rounded-lg p-10 shadow-lg">
+        
+        <h1 class="text-white font-bold text-lg mb-1">Nachma Olivia</h1>
+        
+        <p class="text-neutral-400 text-sm mb-4">20230140048</p>
+        
+        <button class="bg-white text-black text-sm font-semibold py-2 px-4 rounded hover:bg-gray-200 transition">
+            Modul Pertemuan 1
+        </button>
+
+    </div>
+
+</body>
+</html>
